fbee561655333e6757e6d320bb063cb51d857cb6
lizhenhua <lzh1285959868@126.com>
2022-10-18 11:54:13 +0800

    第一次提

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+//破解idea   vscode   可以学vue
+        //安装jdk
+        //Maven不装了
+//        安装mysql    navicate
+        //安装redis
+
+    }
+
+}
